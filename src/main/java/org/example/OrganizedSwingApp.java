ed0138571b58b5f2834be7f49646ddb72dc1e25c
awabqais <awab.qais@gmail.com>
2025-11-09 15:47:52 +0300

    Commit#2
    in the NV we declare alot of variable

---
+++ src/main/java/org/example/OrganizedSwingApp.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrganizedSwingApp {
+    JFrame MainFrame;
+    JPanel PanelMain;
+    CardLayout cardLayout;
+
+    // Store users
+    Map<String, User> users = new HashMap<>();
+}
+
